1c55d3bd9c07bd7bf72ab170ba0c3a06470b0b12
A A Maruf <aamaruf131@gmail.com>
2022-07-16 01:57:40 +0600

    db repository entity added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    TypeOrmModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'reshop',
+      password: '737467',
+      database: 'nestjs-task-management-db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
